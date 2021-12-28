5cdbdeddfa185dd3b0e902242043dc33ffc0f825
laikasin <laikasin93@yahoo.com.hk>
2021-12-29 00:54:10 +0800

    aabc

--- test/contract-test.js
+++ test/contract-test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("RPS", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Accept int amount as bet value", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
