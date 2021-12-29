79f15ee1c5ad2c4905a09d592b407d0f4c34b441
laikasinjason <laikasin93@yahoo.com.hk>
2021-12-30 00:36:35 +0800

    abc

---
+++ contracts/RPS.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract RPS {
+    constructor() {
+    }
+
+    function bet(uint16 memory _greeting) public returns (uint16 memory) {
+        
+    }
+}
--- test/contract-test.js
+++ test/contract-test.js
@@ -2,33 +2,48 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("RPS", function () {
-  it("Accept int amount as bet value", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  describe("Bet", function () {
+    it("Accept int amount as bet value", async function () {
+      const Greeter = await ethers.getContractFactory("Greeter");
+      const greeter = await Greeter.deploy("Hello, world!");
+      await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+      expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+      const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+      // wait until the transaction is mined
+      await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+      expect(await greeter.greet()).to.equal("Hola, mundo!");
+    });
+
+    it("Reject if amount larger than half of dealer", async function () {
+    });
+
+    it("Get nothing back when lose", async function () {
+    });
+
+    it("Get twice amount when win", async function () {
+    });
+
+    it("Return same amount when tie", async function () {
+    });
   });
 
-  it("Accept int amount as bet value", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  describe("Fomo pool", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    it("5% of that goes into a fomo pool", async function () {
+    });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    it(" timer is started and set to end at 1 hour from bet placement time", async function () {
+    });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    it("extend the timer to 1 hour from the bet placement time if the bet amount is at least 10% of the pool size", async function () {
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    });
+    it("fomo timer goes to zero, the last bettor that either turned on or extended the timer wins all the amount in the fomo pool", async function () {
+
+    });
+
   });
-});
